d3f5de2b8839e3e3df55b694293a4fd63c005b32
SMARTIUM <dev@smartium.io>
2020-08-08 01:10:46 -0300

    Implementação do vídeo do professor.

--- imports/client/templates/hello/hello.js
+++ imports/client/templates/hello/hello.js
@@ -16,7 +16,7 @@ var localTracks = {
 var remoteUsers = {};
 // Agora client options
 var options = {
-  appid: null,
+  appid: "3f52f4ef54d4493986689268f42355bd",
   channel: null,
   uid: null,
   token: null,
@@ -28,160 +28,132 @@ Template.hello.onCreated(()=> {
 
 Template.hello.onRendered(()=> {
   if (FlowRouter.getRouteName() === 'sala-de-aula') {
+    if (FlowRouter.getParam('tipoUsuario') === 'professor') {
+      options.role = "host"
+    }
+    else if (FlowRouter.getParam('tipoUsuario') === 'aluno') {
+      options.role = "audience"
+      options.appid = "3f52f4ef54d4493986689268f42355bd";
+      options.token = null;
+    }
 
+    options.channel = FlowRouter.getParam('idSala');
+    join();
+
+    $("#leave").click(function (e) {
+      leave();
+    })
+
+
+  } // if URL de sala de aula
+  else {
+    // alert('A URL deve estar correta.')
   }
-  // the demo can auto join channel with params in url
-  $(() => {
-    var urlParams = new URL(location.href).searchParams;
-    options.appid = urlParams.get("appid");
-    options.channel = urlParams.get("channel");
-    options.token = urlParams.get("token");
-    if (options.appid && options.channel) {
-      $("#appid").val(options.appid);
-      $("#token").val(options.token);
-      $("#channel").val(options.channel);
-      $("#join-form").submit();
-    }
-  })
+});
 
-  $("#host-join").click(function (e) {
-    options.role = "host"
-  })
+Template.hello.helpers({
+  rootUrl() {
+    Meteor.call('get.url', (err, res)=> {
+      rootUrl.set(res);
+    });
+    return rootUrl.get();
+  },
 
-  $("#audience-join").click(function (e) {
-    options.role = "audience"
-  })
-
-  $("#join-form").submit(async function (e) {
-    e.preventDefault();
-    $("#host-join").attr("disabled", true);
-    $("#audience-join").attr("disabled", true);
-    try {
-      options.appid = $("#appid").val();
-      options.token = $("#token").val();
-      options.channel = $("#channel").val();
-      await join();
-      if (options.role === "host") {
-        $("#success-alert a").attr("href", `?appid=${options.appid}&channel=${options.channel}&token=${options.token}`);
-        if(options.token) {
-          $("#success-alert-with-token").css("display", "block");
-        } else {
-          $("#success-alert a").attr("href", `index.html?appid=${options.appid}&channel=${options.channel}&token=${options.token}`);
-          $("#success-alert").css("display", "block");
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      $("#leave").attr("disabled", false);
-    }
-  })
-
-  $("#leave").click(function (e) {
-    leave();
-  })
-
-  async function join() {
-    // create Agora client
-    client = AgoraRTC.createClient({ mode: "live", codec: "h264", role: options.role });
-
-    if (options.role === "audience") {
-      // add event listener to play remote tracks when remote user publishs.
-      client.on("user-published", handleUserPublished);
-      client.on("user-unpublished", handleUserUnpublished);
-    }
-
-    // join the channel
-    options.uid = await client.join(options.appid, options.channel, options.token || null);
-
-    if (options.role === "host") {
-      // create local audio and video tracks
-      localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-      localTracks.videoTrack = await AgoraRTC.createCameraVideoTrack();
-      // play local video track
-      localTracks.videoTrack.play("local-player");
-      $("#local-player-name").text(`localTrack(${options.uid})`);
-      // publish local tracks to channel
-      await client.publish(Object.values(localTracks));
-      // console.log("publish success");
+  dadosDaAula() {
+    return {
+      idSala: FlowRouter.getParam('idSala'),
+      nomeSala: FlowRouter.getParam('nomeSala'),
+      tipoUsuario: FlowRouter.getParam('tipoUsuario'),
+      idUsuario: FlowRouter.getParam('idUsuario'),
+      nomeUsuario: FlowRouter.getParam('nomeUsuario')
     }
   }
+});
 
-  async function leave() {
-    for (trackName in localTracks) {
-      var track = localTracks[trackName];
-      if(track) {
-        track.stop();
-        track.close();
-        localTracks[trackName] = undefined;
-      }
-    }
+Template.hello.events({
+});
 
-    // remove remote users and player views
-    remoteUsers = {};
-    $("#remote-playerlist").html("");
 
-    // leave the channel
-    await client.leave();
 
-    $("#local-player-name").text("");
-    $("#host-join").attr("disabled", false);
-    $("#audience-join").attr("disabled", false);
-    $("#leave").attr("disabled", true);
-    // console.log("client leaves channel success");
+async function join() {
+  // create Agora client
+  client = AgoraRTC.createClient({ mode: "live", codec: "h264", role: options.role });
+
+  if (options.role === "audience") {
+    // add event listener to play remote tracks when remote user publishs.
+    client.on("user-published", handleUserPublished);
+    client.on("user-unpublished", handleUserUnpublished);
+    $('#local-player').parent().remove();
   }
 
-  async function subscribe(user, mediaType) {
-    const uid = user.uid;
-    // subscribe to a remote user
-    await client.subscribe(user, mediaType);
-    // console.log("subscribe success");
-    if (mediaType === 'video') {
-      const player = $(`
-        <div id="player-wrapper-${uid}">
-        <p class="player-name">remoteUser(${uid})</p>
-        <div id="player-${uid}" class="player"></div>
-        </div>
-        `);
-        $("#remote-playerlist").append(player);
-        user.videoTrack.play(`player-${uid}`);
-      }
-      if (mediaType === 'audio') {
-        user.audioTrack.play();
-      }
+  // join the channel
+  options.uid = await client.join(options.appid, options.channel, options.token || null);
+
+  if (options.role === "host") {
+    // create local audio and video tracks
+    localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    localTracks.videoTrack = await AgoraRTC.createCameraVideoTrack();
+    // play local video track
+    localTracks.videoTrack.play("local-player");
+    // $("#local-player-name").text(`localTrack(${options.uid})`);
+    // publish local tracks to channel
+    await client.publish(Object.values(localTracks));
+    // console.log("publish success");
+  }
+}
+
+async function leave() {
+  for (trackName in localTracks) {
+    var track = localTracks[trackName];
+    if(track) {
+      track.stop();
+      track.close();
+      localTracks[trackName] = undefined;
     }
+  }
 
-    function handleUserPublished(user, mediaType) {
-      const id = user.uid;
-      remoteUsers[id] = user;
-      subscribe(user, mediaType);
+  // remove remote users and player views
+  remoteUsers = {};
+  $("#remote-playerlist").html("");
+
+  // leave the channel
+  await client.leave();
+
+  $("#local-player-name").text("");
+  $("#host-join").attr("disabled", false);
+  $("#audience-join").attr("disabled", false);
+  $("#leave").attr("disabled", true);
+  // console.log("client leaves channel success");
+}
+
+async function subscribe(user, mediaType) {
+  const uid = user.uid;
+  // subscribe to a remote user
+  await client.subscribe(user, mediaType);
+  // console.log("subscribe success");
+  if (mediaType === 'video') {
+    const player = $(`
+      <div id="player-wrapper-${uid}">
+      <!-- <p class="player-name">remoteUser(${uid})</p> -->
+      <div id="player-${uid}" class="player"></div>
+      </div>
+      `);
+      $("#remote-playerlist").append(player);
+      user.videoTrack.play(`player-${uid}`);
     }
-
-    function handleUserUnpublished(user) {
-      const id = user.uid;
-      delete remoteUsers[id];
-      $(`#player-wrapper-${id}`).remove();
+    if (mediaType === 'audio') {
+      user.audioTrack.play();
     }
-  });
+  }
 
-  Template.hello.helpers({
-    rootUrl() {
-      Meteor.call('get.url', (err, res)=> {
-        rootUrl.set(res);
-      });
-      return rootUrl.get();
-    },
+  function handleUserPublished(user, mediaType) {
+    const id = user.uid;
+    remoteUsers[id] = user;
+    subscribe(user, mediaType);
+  }
 
-    dadosDaAula() {
-      return {
-        idSala: FlowRouter.getParam('idSala'),
-        nomeSala: FlowRouter.getParam('nomeSala'),
-        tipoUsuario: FlowRouter.getParam('tipoUsuario'),
-        idUsuario: FlowRouter.getParam('idUsuario'),
-        nomeUsuario: FlowRouter.getParam('nomeUsuario')
-      }
-    }
-  });
-
-  Template.hello.events({
-  });
+  function handleUserUnpublished(user) {
+    const id = user.uid;
+    delete remoteUsers[id];
+    $(`#player-wrapper-${id}`).remove();
+  }
